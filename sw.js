b617cf12b9a47a3613f85f33e279a3b417dbdccd
whitefacexd-alt <whiteface.xd@gmail.com>
2026-05-25 14:25:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,20 +1,28 @@
-const CACHE = 'rentbook-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE = "rentbook-v1";
+const ASSETS = [
+  "/RentBook/",
+  "/RentBook/index.html",
+  "/RentBook/manifest.json",
+  "/RentBook/icon-192.png",
+  "/RentBook/icon-512.png"
+];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request))
   );
 });
